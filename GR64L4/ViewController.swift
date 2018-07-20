5513e63410730bc7a2e5d43dd02de31712838c24
Tima_a <Plakhovtima@gmail.com>
2018-07-20 20:50:28 +0300

    test

--- GR64L4/ViewController.swift
+++ GR64L4/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     
 
-        name(name: "Plakhov")
+        name(name: "Timothy")
         
     }
     
